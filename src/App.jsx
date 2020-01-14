8d7928f8ac4ec31dace5cc4d2f65b24fbd7ebfb9
berrocalesjames <berrocalesjames@gmail.com>
2020-01-13 22:43:42 -0800

    Changed App to class-component

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,31 @@ import './App.css';
 import Button from './components/Buttons';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Button title="Press Me!!" />
-      <Button title="Tap Me!!" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+  render() {
+    return (
+      <div className="App">
+        <Button title="Press Me!!" />
+        <Button title="Tap Me!!" />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            James
+            <code>src/App.js</code>
+            and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 
