c9e815fd392e8502466203c58a99128c8ec150ea
berrocalesjames <berrocalesjames@gmail.com>
2020-01-18 11:31:44 -0500

    Moved state from App.jsx to Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Buttons';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.buttonStateChanger = this.buttonStateChanger.bind(this);
-    console.log('Component started');
-    this.state = { pressed: true };
-  }
+class App extends PureComponent {
   // state = {
   //   pressed: false,
   // };
 
-  buttonStateChanger(e) {
-    e.preventDefault();
-    e.target.style.background = 'red';
-    const { pressed } = this.state;
-    this.setState({ pressed: !pressed });
-    // this.setState({ pressed: !this.state.pressed });
-    console.log(`State change to ${pressed}!`);
-    console.log(this.state);
-  }
-
   render() {
     return (
       <div className="App">
-        <Button title="Press Me!!" clicked={this.buttonStateChanger} />
-        <Button title="Tap Me!!" />
+        <Button />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,10 +2,30 @@ import React, { PureComponent } from 'react';
 import './Button.css';
 
 class Button extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.buttonStateChanger = this.buttonStateChanger.bind(this);
+    console.log('Component started');
+    this.state = { pressed: true };
+  }
+
+  buttonStateChanger(e) {
+    e.preventDefault();
+    e.target.style.background = 'red';
+    const { pressed } = this.state;
+    this.setState({ pressed: !pressed });
+    // this.setState({ pressed: !this.state.pressed });
+    console.log(`State change to ${pressed}!`);
+    console.log(this.state);
+  }
+
+
   render() {
     return (
       <div>
-        <button type="button" className="initialButton" onClick={this.props.clicked}>{this.props.title}</button>
+        <button type="button" className="initialButton" onClick={this.buttonStateChanger}>Press Me!</button>
+        <br />
+        <button type="button" className="initialButton" onClick={this.buttonStateChanger}>Tap Me!!</button>
       </div>
     );
   }
