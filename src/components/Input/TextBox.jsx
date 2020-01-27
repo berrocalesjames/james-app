bca8cf2d973427f298cc74cee3a5ae4111d63c84
berrocalesjames <berrocalesjames@gmail.com>
2020-01-27 13:30:23 -0500

    Cleaned up TextBox component code. Still need to address three eslint warnings

--- src/components/Input/TextBox.jsx
+++ src/components/Input/TextBox.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const TextBox = (props) => (
-  <form>
-    <label>
-        Username:
-      <input
-        type="text"
-        id="username"
-        placeholder="Type Text here"
-        onChange={props.changed}
-      />
-    </label>
-  </form>
-);
+const TextBox = (props) => {
+  const { changed } = props;
+  return (
+    <form>
+      <label>
+          Username:
+        <input
+          type="text"
+          id="username"
+          placeholder="Type Text here"
+          onChange={changed}
+        />
+      </label>
+    </form>
+  );
+};
 
 export default TextBox;
